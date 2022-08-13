38c5038a963d0ab2a72a09c2ca8a5aeea402750e
jonas-borf <jonasbarfield@gmail.com>
2022-08-13 12:59:03 -0400

    Final

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,20 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UiService } from '../../services/ui.service';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  title: string = 'task tracker';
+  title: string = 'Task Tracker';
 
-  constructor() { }
+  showAddTask!: boolean;
+  subsciption!: Subscription
+
+  constructor(private uiService:UiService, private router:Router) {
+    this.subsciption = this.uiService.onToggle().subscribe(value => this.showAddTask = value);
+  }
 
   ngOnInit(): void {
   }
 
 
   toggleAddTask() {
-    console.log('toggle');
+    this.uiService.toggleAddTask();
   }
+
+  hasRoute(route:string) {
+    return this.router.url === route;
+   }
 }
--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Task } from '../../Task';
 import { faTimes } from '@fortawesome/free-solid-svg-icons'
 @Component({
@@ -8,12 +8,21 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons'
 })
 export class TaskItemComponent implements OnInit {
   faTimes = faTimes;
-
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
+  @Output() onToggleReminder: EventEmitter<Task> = new EventEmitter();
   @Input()
   task!: Task;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onDelete(task: any) {
+    this.onDeleteTask.emit(task);
+  }
+
+  onToggle(task: Task) {
+    this.onToggleReminder.emit(task);
   }
 
 }
